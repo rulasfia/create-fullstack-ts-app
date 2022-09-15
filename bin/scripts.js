add9a97eac404f94e964e4062164ec4986735a80
Khoirul Asfian <mk.asfian@gmail.com>
2022-09-15 14:07:18 +0700

    feat: update version to 1.0.3

--- bin/scripts.js
+++ bin/scripts.js
@@ -5,36 +5,37 @@ const templateRepo = "https://github.com/rulasfia/create-fullstack-ts-app";
  * @typedef {'win32' | 'linux' | 'darwin' | 'android' | ''} OsType
  */
 
-/**  
+/**
  * clone the repo
  * @param {string} dir - directory to clone the repo
  */
 const gitCloneCommand = (dir) => `git clone --depth 1 ${templateRepo} ${dir}`;
 
-/**  
+/**
  * install dependencies
  * @param {string} dir - project directory
  */
 const installDepsCommand = (dir) => `cd ${dir} && npm install`;
 
-/**  
+/**
  * remove bin file & git history
  * @param {string} dir - project directory
  */
-const cleanupCommand = (dir) => `cd ${dir} && npx shx rm -rf ./bin && npx shx rm -rf .git`;
+const cleanupCommand = (dir) =>
+  `cd ${dir} && npx shx rm -rf ./bin && npx shx rm -rf .git`;
 
-/**  
+/**
  * initialize git
- * @param {string} dir - project directory 
+ * @param {string} dir - project directory
  */
 const initGitCommand = (dir) => `cd ${dir} && git init`;
 
-/**  
- * apply prisma migration  
+/**
+ * apply prisma migration
  * @param {string} dir - project directory
  */
-const postInstallCommnad = (dir) => `cd ${dir}/apps/server/prisma && npx shx touch database.db && cd .. && npx prisma migrate dev --name init && npx prisma generate`;
-
+const postInstallCommnad = (dir) =>
+  `cd ${dir}/apps/server/prisma && npx shx touch database.db && cd .. && npx prisma migrate dev --name init && npx prisma generate`;
 
 module.exports = {
   gitCloneCommand,
@@ -42,4 +43,4 @@ module.exports = {
   cleanupCommand,
   initGitCommand,
   postInstallCommnad,
-}
+};
